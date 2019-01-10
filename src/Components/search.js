91713c1c0b0d79d2da98496b30a0a1c95a1f6d1d
DavidUllauri <30758544+DavidUllauri@users.noreply.github.com>
2019-01-10 12:21:43 -0500

    set up basics; switching off master

--- src/Components/search.js
+++ src/Components/search.js
@@ -13,33 +13,10 @@ class App extends Component {
 class SearchBar extends Component{
   constructor(props){
     super(props)
-    this.state = {savedfirst_name:"", savedlast_name:"", currentfirst_name:"", currentlast_name:"", isEditing: true}
-    this.handleSave = this.handleSave.bind(this);
-    this.handleCancel = this.handleCancel.bind(this);
-    this.handleClickEdit = this.handleClickEdit.bind(this);
-    this.updateFirst = this.updateFirst.bind(this);
-    this.updateLast = this.updateLast.bind(this);
+
   }
 
-  handleSave(event){
-    if(this.state.currentfirst_name !== this.state.savedfirst_name){
-      this.setState({savedfirst_name: this.state.currentfirst_name});
-    }
-    if(this.state.currentlast_name !== this.state.savedlast_name){
-      this.setState({savedlast_name: this.state.currentlast_name});
-    }
-    this.setState({isEditing: false});
-  }
-  handleCancel(){
-    this.setState({currentfirst_name: this.state.savedfirst_name, currentlast_name: this.state.savedlast_name, isEditing:false});
-  }
-  handleClickEdit(){
-    this.setState({currentfirst_name: this.state.savedfirst_name, currentlast_name: this.state.savedlast_name, isEditing: true });
-  }
-  updateFirst(event){
-    this.setState({currentfirst_name: event.target.value});
-  }
-  updateLast(event){
+  handleClick(event){
     this.setState({currentlast_name: event.target.value});
   }
 
@@ -50,7 +27,7 @@ class SearchBar extends Component{
       <tr>
       <td><strong><h4> <form>
       <input id="search-bar" name="first_name" type="text" value={this.state.currentfirst_name} onChange={this.updateFirst} /><br /> <br />
-      <button id="button" onClick={this.handleSave}> Find my gif! </button>
+      <button id="button" onClick={this.handleClick}> Find my gif! </button>
       </form></h4></strong></td>
       </tr>
       </table>
